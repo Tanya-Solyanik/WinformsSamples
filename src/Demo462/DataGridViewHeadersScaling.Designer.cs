4432e4d345950467cd49a203b8e3f5381239a9fa
redmond\tayaso <tanyaso@microsoft.com>
2017-02-15 09:59:02 -0800

    This version of the demo was used in TechReady 2017

---
+++ src/Demo462/DataGridViewHeadersScaling.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Demo462
+{
+    partial class DataGridViewHeadersScaling
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            this.currentDpiLabel = new System.Windows.Forms.Label();
+            this.infoLabel = new System.Windows.Forms.Label();
+            this.exceptionButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(-1, 1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(368, 119);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 73;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 54;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 54;
+            // 
+            // currentDpiLabel
+            // 
+            this.currentDpiLabel.AutoSize = true;
+            this.currentDpiLabel.Location = new System.Drawing.Point(12, 136);
+            this.currentDpiLabel.Name = "currentDpiLabel";
+            this.currentDpiLabel.Size = new System.Drawing.Size(62, 13);
+            this.currentDpiLabel.TabIndex = 3;
+            this.currentDpiLabel.Text = "Current DPI";
+            // 
+            // infoLabel
+            // 
+            this.infoLabel.Location = new System.Drawing.Point(-1, 161);
+            this.infoLabel.Name = "infoLabel";
+            this.infoLabel.Size = new System.Drawing.Size(368, 45);
+            this.infoLabel.TabIndex = 4;
+            this.infoLabel.Text = "Row headers are too narrow to display DPI-scaled icons, but they can be resized m" +
+    "anually or programmatically.";
+            // 
+            // exceptionButton
+            // 
+            this.exceptionButton.Location = new System.Drawing.Point(158, 125);
+            this.exceptionButton.Name = "exceptionButton";
+            this.exceptionButton.Size = new System.Drawing.Size(209, 23);
+            this.exceptionButton.TabIndex = 5;
+            this.exceptionButton.Text = "Open Exception Dialog";
+            this.exceptionButton.UseVisualStyleBackColor = true;
+            this.exceptionButton.Click += new System.EventHandler(this.exceptionButton_Click);
+            // 
+            // DataGridViewHeadersScaling
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 203);
+            this.Controls.Add(this.exceptionButton);
+            this.Controls.Add(this.infoLabel);
+            this.Controls.Add(this.currentDpiLabel);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "DataGridViewHeadersScaling";
+            this.Text = "DataGridView Row Headers";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label currentDpiLabel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewButtonColumn Column2;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Column3;
+        private System.Windows.Forms.Label infoLabel;
+        private System.Windows.Forms.Button exceptionButton;
+    }
+}
---
+++ src/Demo462/DataGridViewHeadersScaling.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Demo462
+{
+    public partial class DataGridViewHeadersScaling : Form
+    {
+        #region Developer has to calculate DPI valus by P/Invoking windows API
+        [DllImport("User32", ExactSpelling = true, EntryPoint = "GetDC", CharSet = CharSet.Auto)]
+        private static extern IntPtr GetDC(HandleRef hWnd);
+
+        [DllImport("Gdi32", SetLastError = true, ExactSpelling = true, CharSet = CharSet.Auto)]
+        private static extern int GetDeviceCaps(HandleRef hDC, int nIndex);
+
+        [DllImport("User32", ExactSpelling = true, EntryPoint = "ReleaseDC", CharSet = CharSet.Auto)]
+        private static extern int ReleaseDC(HandleRef hWnd, HandleRef hDC);
+
+        private int deviceDpi;
+        #endregion
+
+        private void InitializeDpi()
+        {
+            IntPtr hDC = GetDC(new HandleRef(null, IntPtr.Zero));
+            if (hDC != IntPtr.Zero)
+            {
+                deviceDpi = GetDeviceCaps(new HandleRef(null, hDC), 88);
+
+                ReleaseDC(new HandleRef(null, IntPtr.Zero), new HandleRef(null, hDC));
+            }
+        }
+
+        public DataGridViewHeadersScaling()
+        {
+            InitializeComponent();
+
+            // AutoSizeMode for columns is set to ColumnHeader
+            dataGridView1.Rows[0].ErrorText = "error text";
+            dataGridView1.Rows.Add(new object[] { "aaa", "bbbb" });
+            dataGridView1.Rows.Add(new object[] { "baaa", "abbbb" });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.RightToLeft = (dataGridView1.RightToLeft == RightToLeft.Yes ? RightToLeft.No: RightToLeft.Yes);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            InitializeDpi();
+            currentDpiLabel.Text = $"Current scalling = {(int)Math.Round(deviceDpi / 96.0) * 100}%";
+
+            // Resize row header width for DPI.
+            dataGridView1.RowHeadersWidth *= (int)Math.Round(deviceDpi / 96.0);
+        }
+
+        private void exceptionButton_Click(object sender, EventArgs e)
+        {
+            (new ThreadExceptionDialog(new Exception("Really long exception description string, because we want to see if it properly wraps around or is truncated."))).Show();
+        }
+    }
+}
